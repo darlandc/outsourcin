15c2b7e4f2a550fe1e2182855b3aed5cd20a6b61
Darlan de Castro <darlandc@gmail.com>
2020-03-17 00:58:42 -0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'outsourcin';
+  helloMessage: string;
+
+  data = [];
+  username: any;
+  level: any;
+  constructor(private http: HttpClient) {
+    // const API_URL = 'http://localhost:8888/outsourcin/employee.php';
+    const API_URL = 'http://localhost:3000/';
+    this.http.get(API_URL).subscribe(data => {
+    this.data.push(data);
+    this.helloMessage = this.data[0].welcome;
+    this.username = this.data[0].currentUserData.username;
+    this.level = this.data[0].currentUserData.level;
+    // console.log(this.data[0]);
+
+
+    }, error => console.error(error));
+  }
+
+
+
+
 }
